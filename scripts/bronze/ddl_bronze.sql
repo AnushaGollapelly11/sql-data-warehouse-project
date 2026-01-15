11c6d7b5fb57216ef195d8a88745255aae5cafd3
AnushaGollapelly11 <159449098+AnushaGollapelly11@users.noreply.github.com>
2026-01-15 20:03:34 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,9 +13,9 @@ GO
 CREATE TABLE bronze.crm_cust_info(
 cst_id INT,
 cst_key NVARCHAR(50),
-cst_fisrtname NVARCHAR(50),
+cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
-cst_material_status NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
@@ -83,4 +83,3 @@ cat NVARCHAR(50),
 subcat NVARCHAR(50),
 maintenance NVARCHAR(50)
 );
-
